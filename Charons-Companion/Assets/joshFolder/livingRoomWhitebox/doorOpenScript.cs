5bd5aae0bc633ebf3d4ac7acc6bea9fea85da7c6
Josh Chen <jc74674@g.rit.edu>
2025-02-18 23:41:51 -0500

    start of living room graybox

---
+++ Charons-Companion/Assets/joshFolder/livingRoomWhitebox/doorOpenScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class doorOpenScript : MonoBehaviour
+{
+    [SerializeField] Transform pivotPoint;
+    [SerializeField] AnimationClip anim;
+
+    private void Start()
+    {
+        OpenDoor();
+    }
+
+    private void OpenDoor()
+    {
+        
+    }
+}
