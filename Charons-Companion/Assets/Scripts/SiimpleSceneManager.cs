9e645bdbeda6500309f01abf236a94a0b5e93251
kevinxiao712 <kevinxiao712@gmail.com>
2025-04-06 21:47:12 -0400

    ui sstff

--- Charons-Companion/Assets/Scripts/SiimpleSceneManager.cs
+++ Charons-Companion/Assets/Scripts/SiimpleSceneManager.cs
@@ -1,18 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class SiimpleSceneManager : MonoBehaviour
 {
+    public GameObject imagePanel;
     // Call this to load the next scene in build order
     public void StartGame()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+    private void Start()
+    {
+        // Ensure the panel is initially hidden
+        imagePanel.SetActive(false);
+    }
 
-    // Call this to quit the application
-    public void QuitGame()
+        public void QuitGame()
     {
         Debug.Log("Quit Game");  // This will show in editor
         Application.Quit();      // This will work in a built version
+    }
+    private void Update()
+    {
+        if (imagePanel.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClosePanel();
+        }
+    }
+    public void OpenPanel()
+    {
+        imagePanel.SetActive(true);
+    }
+    public void ClosePanel()
+    {
+        imagePanel.SetActive(false);
     }
 }
