e3f88369d29eea7ad028ce7d4f4979b5d2d035c4
kevinxiao712 <kevinxiao712@gmail.com>
2025-04-10 20:16:29 -0400

    flame

---
+++ Charons-Companion/Assets/Scripts/SphereTriggerSound.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class TriggerToggleObjects : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    [Header("Objects to Toggle")]
+    public GameObject firstObject;  // initially enabled
+    public GameObject secondObject; // initially disabled
+
+    private void Start()
+    {
+
+        SphereCollider sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.isTrigger = true;
+
+        if (firstObject != null)
+            firstObject.SetActive(true);
+        if (secondObject != null)
+            secondObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (audioSource != null && audioClip != null)
+            {
+                audioSource.PlayOneShot(audioClip);
+            }
+            else
+            {
+                Debug.LogWarning("AudioSource or AudioClip not set on " + gameObject.name);
+            }
+
+            if (firstObject != null)
+                firstObject.SetActive(false);
+            if (secondObject != null)
+                secondObject.SetActive(true);
+        }
+    }
+}
