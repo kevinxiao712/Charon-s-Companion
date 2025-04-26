3ac2f5858a9a69300c0b0ba92cbaa761670d4ca2
Zejun <zm8298@g.rit.edu>
2025-04-25 23:52:28 -0400

    endscreen and creditscreen done

---
+++ Charons-Companion/Assets/Scenes/UI/EndScreen.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartBTN()
+    {
+        SceneManager.LoadScene("Living Room Scene"); 
+    }
+
+    public void MainMenuBTN()
+    {
+        SceneManager.LoadScene("MainMenuFinished"); 
+    }
+
+    public void GoToCreditScreen()
+    {
+        SceneManager.LoadScene("CreditScreen");
+    }
+
+    public void BackToEndScreen()
+    {
+        SceneManager.LoadScene("EndScreen");
+    }
+}
