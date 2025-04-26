4d4a69cfb285efbdae5e4ec43b1025b69a2907c1
Zejun <zm8298@g.rit.edu>
2025-04-26 02:17:24 -0400

    update

--- Charons-Companion/Assets/Scenes/UI/EndScreen.cs
+++ Charons-Companion/Assets/Scenes/UI/EndScreen.cs
@@ -6,7 +6,8 @@ public class EndScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
